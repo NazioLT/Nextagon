4e4d39913c88543969bb9d5f74ed7812e4f04880
Henno Théo <zoupeurletroll@gmail.com>
2023-03-30 21:39:05 +0200

    hexagon bas

---
+++ Assets/Scripts/Runtime/Orientation.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public struct Orientation
+{
+    public Orientation(float _f0, float _f1, float _f2, float _f3,
+                        float _b0, float _b1, float _b2, float _b3, float _startAngle)
+    {
+        f0 = _f0;
+        f1 = _f1;
+        f2 = _f2;
+        f3 = _f3;
+
+        b0 = _b0;
+        b1 = _b1;
+        b2 = _b2;
+        b3 = _b3;
+
+        startAngle = _startAngle;
+    }
+
+    public readonly float f0, f1, f2, f3;
+    public readonly float b0, b1, b2, b3;
+    public readonly float startAngle;
+
+    public static readonly Orientation LayoutPointy = new Orientation(Mathf.Sqrt(3f), Mathf.Sqrt(3f) / 2f, 0f, 3f / 2f,
+                Mathf.Sqrt(3f) / 3f, -1f / 3f, 0f, 2f / 3f, 0.5f);
+
+    public static readonly Orientation LayoutFlat = new Orientation(3f / 2f, 0f, Mathf.Sqrt(3f) / 2f, Mathf.Sqrt(3f),
+                2f / 3f, 0f, -1f / 3f, Mathf.Sqrt(3f) / 3f, 0f);
+}
