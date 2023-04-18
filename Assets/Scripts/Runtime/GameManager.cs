52107c90cb1029345de045d8e342f24399136eb3
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2023-04-18 17:07:53 +0200

    menu select chill or normal mode

--- Assets/Scripts/Runtime/GameManager.cs
+++ Assets/Scripts/Runtime/GameManager.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Nazio_LT.Tools.Core;
 
 public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private GameMode currentGameMode;
 
-    public void SelectGameMode()
+    public void Play(GameMode _gameMode)
     {
-
+        currentGameMode = _gameMode;
+        SceneManager.LoadScene("Game");
     }
 
     public static GameMode GameMode => instance.currentGameMode;
---
+++ Assets/Scripts/Runtime/MainMenu/PlayGameMode.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Nazio_LT.Tools.UI;
+
+public class PlayGameMode : MonoBehaviour
+{
+    [SerializeField] private GameMode gameMode;
+    [SerializeField] private NButton button;
+
+    private void Awake() {
+        if(button == null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        button.onClick.AddListener(() => 
+        {
+            GameManager.instance.Play(gameMode);
+        });
+    }
+}
