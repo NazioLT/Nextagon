63c7da4421a072b53edd545c4536e5db4c1479dd
Henno Théo <zoupeurletroll@gmail.com>
2023-04-24 00:10:41 +0200

    some fix

--- Assets/Scripts/Runtime/Grid/HexagonGrid.cs
+++ Assets/Scripts/Runtime/Grid/HexagonGrid.cs
@@ -215,7 +215,8 @@ public class HexagonGrid : MonoBehaviour
 
     private void OnEndTurn()
     {
-        score.AddScore(selectedCase.Number);
+        if(selectedCase)
+            score.AddScore(selectedCase.Number);
         jumpPower.RemoveCount(jumpInUsing);
     }
 
